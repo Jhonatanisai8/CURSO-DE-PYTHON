3bd8cd3a02d86d074ee1415b4370a523765adf49
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-05 21:03:23 -0500

    Tipo de datos: Boolean Ejemplos

--- Sesion02-Tipos-de-datos/Variables.py
+++ Sesion02-Tipos-de-datos/Variables.py
@@ -9,6 +9,7 @@ print(nombreCompleto)
 # verificamos el tipo de dato de una varible
 print("Tipo de dato de la variable: "+nombre)
 print(type(nombre))
+print("")
 
 
 #tipo int (entero)
@@ -18,10 +19,18 @@ edad += 1
 print("Edad: ",edad)
 print("Tu edad es: "+str(edad))
 print("Tipo de dato de la variable edad: ",type(edad))
-
+print("")
 #tipo float
 altura =  180.5
 print("TIPO DE DATO FLOAT")
 print(altura," cm")
 print("Tipo de dato de la variable altura: ",type(altura))
 print("Tu altura es: "+str(altura)+" cm")
+print("")
+
+#tipo de dato boolean
+print("TIPO DE DATO BOOLEAN")
+humano = False
+print("Es humano: ",humano)
+print("Tipo de dato de la variable Humano: ",type(humano))
+print("Eres un humano: "+str(humano))
