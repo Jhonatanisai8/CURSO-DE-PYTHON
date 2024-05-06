4fc2ac9709f63b318915e11c4ccf4ee214f3f868
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-05 20:59:05 -0500

    Tipo de datos: Float Ejemplo

--- Sesion02-Tipos-de-datos/Variables.py
+++ Sesion02-Tipos-de-datos/Variables.py
@@ -1,3 +1,4 @@
+print("Tipos de datos en Python")
 # tipo String
 nombre = "Jhonatan Isai"
 apellidoSegundo = "Ojeda Sanchez"
@@ -15,4 +16,12 @@ print("TIPO DE DATO INT")
 edad = 18
 edad += 1
 print("Edad: ",edad)
+print("Tu edad es: "+str(edad))
 print("Tipo de dato de la variable edad: ",type(edad))
+
+#tipo float
+altura =  180.5
+print("TIPO DE DATO FLOAT")
+print(altura," cm")
+print("Tipo de dato de la variable altura: ",type(altura))
+print("Tu altura es: "+str(altura)+" cm")
