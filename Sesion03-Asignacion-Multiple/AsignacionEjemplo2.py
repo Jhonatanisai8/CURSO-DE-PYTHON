fbe7ee9301c356214c087546b3d434dacb2ed4c0
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-05 21:15:27 -0500

    Asignacion Multiple

---
+++ Sesion03-Asignacion-Multiple/AsignacionEjemplo2.py
@@ -0,0 +1,17 @@
+nombre1 = 10;
+nombre2 = 10;
+nombre3 = 10;
+nombre4 = 10;
+
+nombre5 = nombre6 = nombre7 = nombre8 = 10
+#Imprimimos
+print(nombre1)
+print(nombre2)
+print(nombre3)
+print(nombre4)
+print("")
+print("Imprecion de Asignacion multiple")
+print(nombre5)
+print(nombre6)
+print(nombre7)
+print(nombre8)
---
+++ Sesion03-Asignacion-Multiple/asignacion.py
@@ -0,0 +1,18 @@
+#ASIGNACION MULTIPLE
+nombre1,edad1,atractivo1 = "Jhonatan",18,True
+
+
+#ASIGNACION NORMAL
+nombre = "Jhonatan"
+edad = 18
+atractivo = True
+print("Impresion comun de las variables")
+print("Nombre : "+nombre)
+print("Edad: ",edad)
+print("Es Atrativo: ",atractivo)
+print("")
+#imprimimos
+print("Impresion con Asignacion Multiple")
+print("Nombre: "+nombre1)
+print("Edad: ",edad1)
+print("Es Atractivo: ",atractivo1)
