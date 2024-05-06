9e305bb6d4fed6abde13619385f708b93631c197
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-05 20:47:04 -0500

    Tipos de datos: String ejemplos

---
+++ Sesion02-Tipos-de-datos/Variables.py
@@ -0,0 +1,11 @@
+# tipo String
+nombre = "Jhonatan Isai"
+apellidoSegundo = "Ojeda Sanchez"
+nombreCompleto = "Hola mi nombre es: "+nombre + " "+apellidoSegundo
+print("Hola mi nombre es "+nombre+" "+apellidoSegundo)
+print(nombreCompleto)
+
+# verificamos el tipo de dato de una varible
+print("Tipo de dato de la variable: "+nombre)
+print(type(nombre))
+
