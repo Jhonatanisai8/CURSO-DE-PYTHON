cb364be2ce03b8536e2fd46de4c247f67fe84045
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-02 00:36:43 -0500

    Sesion 05: Convertir Tipos de Datos

---
+++ Sesion05-Convertir-Tipos-de-datos/converir.py
@@ -0,0 +1,31 @@
+print("Hoy aprenderemos a convertir tipos de datos: ")
+
+#variables
+x = 34
+y = 3.5
+z = "67"
+
+#mostramos
+print(x)
+print(y)
+print(z)
+
+#convertimos
+print("Covertimos")
+print(int(y)) #convertimos a entero
+print(str(x)) #convertimos a String
+print(float(z)) #convertimos a decimal
+
+print("Guardando el valor convetido en la misma variable")
+ #para guardar el cambio dentro de la variable
+numero = 123
+sueldo = 1200.56
+edad = 34
+numero = str(numero) 
+sueldo = int(sueldo)
+edad = float(edad)
+
+print(numero)
+print(sueldo)
+print(edad)
+print("La edad es: "+str(edad))
