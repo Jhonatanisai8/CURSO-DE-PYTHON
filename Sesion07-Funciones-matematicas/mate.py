71ff0a27c39cfd3df37dc3f49760c4c3cc5e92cb
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-02 00:59:22 -0500

    Sesion 07: Funciones matemáticas

---
+++ Sesion07-Funciones-matematicas/mate.py
@@ -0,0 +1,39 @@
+import math
+
+PI = 3.14
+print("Redondeado la variable Pi: "+str(PI))
+print(round(PI))
+print("")
+
+print("Redondeado un numero hacia arriba la variable Pi: "+str(PI))
+print(math.ceil(PI))
+print("")
+
+print("Redondeado un numero hacia abajo la variable Pi: "+str(PI))
+print(math.floor(PI))
+print("")
+
+print("valor absoluto de la variable Pi: "+str(PI))
+print(abs(PI))
+print("")
+
+print("Elevar a un exponente la variable Pi: "+str(PI))
+print(pow(PI,2))
+print("")
+
+print("Raiz cuadrada de 9")
+print(math.sqrt(9))
+print("")
+
+#metodo para sabe el maximo y minimo
+numeroA = 3
+numeroB = 8
+numeroC = 5
+print("Funcion para saber el maximo")
+print(max(numeroA,numeroB,numeroC))
+print("")
+
+print("Funcion para saber el minimo")
+print(min(numeroA,numeroB,numeroC))
+print("")
+
